91168e31253530a8de27c5d6101506ed27e7e9a9
Sarah Kuny <sarahkuny@icloud.com>
2022-01-09 19:48:18 -0600

    add hex js functionality

---
+++ hex.js
@@ -0,0 +1,20 @@
+  const btn = document.getElementById('btn');
+  
+  const hex = [0 ,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 , 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+ 
+  const color = document.querySelector('.color');
+
+  btn.addEventListener('click', function () {
+    let hexKey = '#';
+    for (let i = 0; i < 6; i++ ) {
+        let randomNumber = getRandomNumber();
+
+        hexKey += hex[randomNumber]
+    }  
+    document.body.style.backgroundColor = hexKey;
+    color.innerHTML = hexKey;
+  });
+
+  function getRandomNumber () {
+    return Math.floor(Math.random()* hex.length);
+}
